374f16ea4142e62076509304f7d1d7c467f42ad8
Delmiro Ribeiro <delmirojunior2020@gmail.com>
2020-07-26 20:04:46 -0300

    adicionando multiplicação

---
+++ multiplicando.py
@@ -0,0 +1,4 @@
+m1 = float(input('Digite o valor que deseja multiplicar : '))
+m2 = float(input('Digite o outro valor : '))
+rm = m1 * m2
+print('O resultado da multiplicação entre {} e {} é {}'.format(m1,m2,rm))
