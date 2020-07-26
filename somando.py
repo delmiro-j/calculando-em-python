0d5dfcdcf5090aae4cee0db2abdd89843d25ff84
Delmiro Ribeiro <delmirojunior2020@gmail.com>
2020-07-26 19:48:58 -0300

    base de soma

---
+++ somando.py
@@ -0,0 +1,4 @@
+s1 = float(input('Digite o número que você deseja somar : '))
+s2 = float(input('Digite o outro valor : '))
+r1 = s1 + s2
+print('A soma entre {} e {} é igual a {}'.format(s1,s2,r1))
