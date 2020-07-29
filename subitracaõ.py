6db77441dad59c622717c4949e82c6b96deda1f2
Delmiro Ribeiro <delmirojunior2020@gmail.com>
2020-07-28 21:16:58 -0300

    add subtração

---
+++ subitracaõ.py
@@ -0,0 +1,4 @@
+sb1 = float(input('Digite o número que você deseja subtrair : '))
+sb2 = float(input('Digite o outro valor : '))
+rs1 = sb1 - sb2
+print('A subtração entre {} e {} é igual a {}'.format(sb1,sb2,rs1))
